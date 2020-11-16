e5518e64082aa49f52322d59b4b7792666e5a987
Grant Walton <waltogra13@gmail.com>
2020-11-16 04:37:59 -0800

    Everything running smoothly. Just need to add description to Readme for first deliverable

--- src/test/java/farmsim/mainTest.java
+++ src/test/java/farmsim/mainTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 
 public class mainTest {
     @Test
-    public void testTestMethod(){
+    public void testTestMethod() {
         Main main = new Main();
         assertEquals(main.testMethod(), true);
     }
